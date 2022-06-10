96d34709dd6507bc69c27b1b9683f4203f1229eb
Aleksandra Rabcewicz - Perceptus <a.rabcewicz@perceptus.pl>
2022-06-10 14:23:52 +0200

    invalidLogin

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import {
 } from "react-router-dom";
 import LoginScrean from "./components/routers/loginScrean/LoginScrean";
 import PatientPage from "./components/routers/patientPage/PatientPage";
+import DoctorPage from "./components/routers/doctorPage/DoctorPage";
+import React, { useState } from 'react';
 
 function App() {
-  
+  const [nextPage, setNextPage] = useState("login");
   return (
     <div id="App">
-      <BrowserRouter>
+      {nextPage === "login" ? <LoginScrean
+        onLoginPatient={() => { setNextPage("parient") }}
+        onLoginDoctor={() => { setNextPage("doctor") }}
+      /> : ""}
+      {nextPage === "parient" ? <PatientPage /> : ""}
+      {nextPage === "doctor" ? <DoctorPage /> : ""}
+    </div>
+  );
+}
+
+export default App;
+/*<BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginScrean/>}>
           </Route>
@@ -22,8 +35,4 @@ function App() {
           
         </Routes>
       </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
+*/
---
+++ src/components/routers/doctorPage/DoctorPage.js
@@ -0,0 +1,13 @@
+import './DoctorPage.css';
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+function DoctorPage({
+}) {
+    return (
+        <div>
+          doctorPage
+        </div>
+    );
+}
+
+export default DoctorPage;
