80984f5b6fe91e0195007c85f70b783687d91dcf
Aleksandra Rabcewicz - Perceptus <a.rabcewicz@perceptus.pl>
2022-06-13 12:15:59 +0200

    OneVisitPlanning

--- src/components/doctor/doctorPlanningVisits/DoctorPlanningVisits.js
+++ src/components/doctor/doctorPlanningVisits/DoctorPlanningVisits.js
@@ -1,24 +1,54 @@
-import React, { useEffect, useState } from 'react';
-//import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import "./DoctorPlanningVisits.css";
-import { AiFillCaretLeft} from "react-icons/ai";
+import { AiFillCaretLeft } from "react-icons/ai";
+import HarmonogramVisitPlanning from "./../harmonogramVisitPlanning/HarmonogramVisitPlanning";
+import OneVisitPlanning from "./../oneVisitPlanning/OneVisitPlanning";
 
 function DoctorPlanningVisits({
    onBack
 }) {
+   const [oneVisitPlanning, setOneVisitPlanning] = useState(false);
+   const [harmonogramVisitPlanning, setHarmonogramVisitPlanning] = useState(false);
 
-  
-   return(
-      <div className="col-12">
-         <div className="col-1 offset-md-1 back" onClick={onBack}> 
-            <AiFillCaretLeft/>
-         </div>
-            <button type="button" className="btn bg-primary text-light col-12 col-lg-4 mt-4 offset-lg-1" onClick={()=>{
-               console.log("szuk")
-            }}>
-               Szukaj
-            </button>
-      </div>
+   return (
+      <Container className="col-12">
+         <Row>
+            <Col className="col-1 offset-md-1 back" onClick={onBack}>
+               <AiFillCaretLeft />
+            </Col>
+         </Row>
+         <Row>
+            <Col className="col-12 col-md-5 my-3 " >
+               <button type="button" className="btn btn-primary col-12 p-2" onClick={() => {
+                  setOneVisitPlanning(true);
+                  setHarmonogramVisitPlanning(false);
+               }}>
+                  Zaplanuj wizytę
+               </button>
+            </Col>
+            <Col className="col-12  col-md-5 my-3 offset-md-2" >
+               <button type="button" className="btn btn-primary col-12 p-2" onClick={() => {
+                  setOneVisitPlanning(false);
+                  setHarmonogramVisitPlanning(true);
+               }}>
+                  Zaplanuj Harmonogram
+               </button>
+            </Col>
+         </Row>
+         {harmonogramVisitPlanning?<HarmonogramVisitPlanning/>:""}
+         {oneVisitPlanning?<OneVisitPlanning/>:""}
+         
+         <Row>
+            <Col className="col-12 my-3 " >
+               <button type="button" className="btn btn-primary col-12 p-2" onClick={() => {
+                  console.log("paln")
+               }}>
+                  Planuj
+               </button>
+            </Col>
+         </Row>
+      </Container>
    )
 }
 
---
+++ src/components/doctor/harmonogramVisitPlanning/HarmonogramVisitPlanning.js
@@ -0,0 +1,17 @@
+import './HarmonogramVisitPlanning.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function HarmonogramVisitPlanning() 
+{
+  return (
+    <Container className="col-12 p-1 patent-operation-vew" >
+      <Row className="col-12 m-1 m-md-5" >
+        <Col className="col-12 col-md-5 my-3" >
+            HarmonogramVisitPlanning
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default HarmonogramVisitPlanning;
