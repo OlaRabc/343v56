011fb868390ccc85c7fd6282be867dada656b790
Aleksandra Rabcewicz - Perceptus <a.rabcewicz@perceptus.pl>
2022-06-14 14:17:43 +0200

    DBCopyAndBackendBeginning

---
+++ Backend/calendar/src/main/java/pl/calendar/calendar/DoctorRepository.java
@@ -0,0 +1,27 @@
+package pl.calendar.calendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class DoctorRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Doctor> getAllDoctors(){
+
+        return jdbcTemplate.query("SELECT doctorId, firstName, lastName, pwz, cityId, specialization, street, localNumber FROM calendar.doctor;", BeanPropertyRowMapper.newInstance(Doctor.class));
+    }
+    public Doctor getDoctorById(Integer doctorId)
+    {
+        return jdbcTemplate.queryForObject("SELECT doctorId, firstName, lastName, pwz, cityId, specialization, street, localNumber FROM calendar.doctor WHERE doctorId=?; ",BeanPropertyRowMapper.newInstance(Doctor.class),doctorId);
+    }
+    public Doctor getDoctorBySpecializationAndCity(String specialization, Integer cityId)
+    {
+        return jdbcTemplate.queryForObject("SELECT doctorId, firstName, lastName, pwz, cityId, specialization, street, localNumber FROM calendar.doctor WHERE specialization=? AND cityId=?; ",BeanPropertyRowMapper.newInstance(Doctor.class),specialization,cityId);
+    }
+}
